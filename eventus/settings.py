2de4134a7a4e88e189b653544c6d59ad38031c04
Hector Riquelme <hector.riquelme1169@gmail.com>
2017-08-19 17:48:20 -0300

    Configuracion inicial

--- eventus/settings.py
+++ eventus/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'app.events',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'eventus.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'eventus',
+        'USER': 'root',
+        'PASSWORD': '',
+        'PORT': 3306,
+        'HOST': '/opt/lampp/var/mysql/mysql.sock'
     }
 }
 
