6497593862d7bd0b79de5125ae1b8562090fcd2f
Hector Riquelme <hector.riquelme1169@gmail.com>
2017-08-30 21:40:47 -0300

    Primera view eventos

--- app/events/admin.py
+++ app/events/admin.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+from .models import Event, Category, Assistant, Comments
 from django.contrib import admin
 
-# Register your models here.
+admin.site.register(Event)
+admin.site.register(Category)
+admin.site.register(Assistant)
+admin.site.register(Comments)
--- app/events/views.py
+++ app/events/views.py
@@ -1,6 +1,14 @@
-# -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from .models import Event, Category
 
-# Create your views here.
+def index(request):
+    events = Event.objects.all().order_by('-created')[:6]
+    categories = Category.objects.all()
+    print events
+    print categories
+    return render(request, 'index.html', {
+        'events': events,
+        'categories': categories
+    })
